8e398e788941eb4af66ef9a20817d6b47ef877eb
nam nguyen <namnguyen@fahasa.com>
2022-01-05 21:51:39 +0700

    add tim thong tin thi sinh va ds thi sinh

--- src/main/java/edu/sgu/qlttngoaingu/service/KhoaThiService.java
+++ src/main/java/edu/sgu/qlttngoaingu/service/KhoaThiService.java
@@ -1,7 +1,9 @@
 package edu.sgu.qlttngoaingu.service;
 
 import edu.sgu.qlttngoaingu.entity.KhoaThi;
+import edu.sgu.qlttngoaingu.entity.PhongThi;
 import edu.sgu.qlttngoaingu.repository.KhoaThiRepository;
+import edu.sgu.qlttngoaingu.repository.PhongThiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,12 @@ import java.util.List;
 public class KhoaThiService {
     @Autowired
     private KhoaThiRepository khoaThiRepository;
+    @Autowired
+    private PhongThiRepository phongThiRepository;
     public List<KhoaThi> getAll(){
         return khoaThiRepository.findAll();
+    }
+    public List<PhongThi> getPhongThi(){
+      return   phongThiRepository.findAll();
     }
 }
--- src/main/java/edu/sgu/qlttngoaingu/service/ThiSinhService.java
+++ src/main/java/edu/sgu/qlttngoaingu/service/ThiSinhService.java
@@ -3,11 +3,14 @@ package edu.sgu.qlttngoaingu.service;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import edu.sgu.qlttngoaingu.dto.DangKiForm;
+import edu.sgu.qlttngoaingu.dto.DanhSachThiSinh;
+import edu.sgu.qlttngoaingu.dto.ThongTinThiSinh;
 import edu.sgu.qlttngoaingu.entity.KhoaThi;
 import edu.sgu.qlttngoaingu.entity.PhieuDangKy;
 import edu.sgu.qlttngoaingu.entity.PhieuDangKyId;
@@ -57,4 +60,51 @@ public class ThiSinhService {
         }
         return null;
     }
+    public List<ThongTinThiSinh> getThogTinThiSinhs(String name, String phone){
+        try {
+            List<Object[]> objects= thiSinhRepository.getThiSinh(name,phone);
+            List<ThongTinThiSinh> result= new ArrayList<>();
+            objects.stream().forEach(s->{
+                ThongTinThiSinh thongTinThiSinh= new ThongTinThiSinh();
+                thongTinThiSinh.setSoBaoDanh(s[0]!=null?s[0].toString():"");
+                thongTinThiSinh.setPhong(s[1]!=null?s[1].toString():"");
+                thongTinThiSinh.setNghe(s[2]!=null?Float.parseFloat(s[2].toString()):0);
+                thongTinThiSinh.setNoi(s[3]!=null?Float.parseFloat(s[3].toString()):0);
+                thongTinThiSinh.setDoc(s[4]!=null?Float.parseFloat(s[4].toString()):0);
+                thongTinThiSinh.setViet(s[5]!=null?Float.parseFloat(s[5].toString()):0);
+                result.add(thongTinThiSinh);
+            });
+            return result;
+        }
+        catch (Exception e){
+
+        }
+        return null;
+    }
+    public List<DanhSachThiSinh>  getDanhSachThiSinh(Integer id,Boolean option){
+        try {
+            List<Object[]> objects= new ArrayList<>();
+           if (option){
+               objects = thiSinhRepository.getThiSinhByKhoaThi(id);
+           }
+           else {
+               objects=thiSinhRepository.getThiSinhByPhongThi(id);
+           }
+            List<DanhSachThiSinh> result= new ArrayList<>();
+            objects.stream().forEach(s->{
+                DanhSachThiSinh danhSachThiSinh= new DanhSachThiSinh();
+                danhSachThiSinh.setTen(s[0]!=null?s[0].toString():"");
+                danhSachThiSinh.setSoBaoDanh(s[1]!=null?s[1].toString():"");
+                danhSachThiSinh.setCmnd(s[2]!=null?s[2].toString():"");
+                danhSachThiSinh.setSoDienThoai(s[3]!=null?s[3].toString():"");
+                result.add(danhSachThiSinh);
+            });
+            return result;
+        }
+        catch (Exception e){
+
+        }
+        return null;
+    }
+
 }
